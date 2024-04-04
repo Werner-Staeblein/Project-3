20a8ea9c7a547aadde671b8d03a34b0352c4f288
Werner <wstablein@hotmail.com>
2024-04-04 13:02:26 +0200

    commit of a test code

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+a = input("Please provide your name: ")
+
+if len(a) > 10:
+    print("Name is too long")
+else:
+    print("Name within limits of characters")
+
