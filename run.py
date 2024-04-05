98207655a078c0c029ee19905e4f82e4be11fa28
Werner <wstablein@hotmail.com>
2024-04-05 09:57:01 +0200

    Remove initial testcode. Verify Heroku connect. Heroku and GH connect confirmed.

--- run.py
+++ run.py
@@ -2,10 +2,3 @@
 
 # the extra line was added with \n
 
-a = input("Please provide your name with maximum of 10 characters:\n")
-
-if len(a) > 10:
-    print("Name is too long")
-else:
-    print("Name within limits of characters")
-
